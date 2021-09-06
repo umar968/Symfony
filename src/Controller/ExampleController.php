c96943ad76d5030aab3cbc3f7c29efedd1155a7e
Muhammad Umar <umar.roulia@gmail.com>
2021-09-06 10:05:53 +0500

    HTTP Req

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -2,25 +2,22 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ExampleController extends AbstractController
 {
     /**
      * @Route("/example/{number}", name="example_controller")
      */
-    public function number($number, LoggerInterface $logger, Session $Session): Response
+    public function number($number, Request $req): Response
     {
-        $logger->info("render number twig");
-        if ($number == 1) {
-            throw $this->createNotFoundException("No number was provided");
-        }
-        $Session->set("number", $number);
-        $this->addFlash("notice", "Number you send was" . $number);
-        return $this->render('example/number.html.twig', ['number' => $number]);
+
+        // return new Response($req->query->get('page'));
+        return $this->json(["name" => $req->query->get("name")]);
     }
 }
