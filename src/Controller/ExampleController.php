b0e59178faa8da2b9d308466b6a188851062d27f
Muhammad Umar <umar.roulia@gmail.com>
2021-09-06 17:01:05 +0500

    Errors Removed

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -2,15 +2,14 @@
 
 namespace App\Controller;
 
-
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class ExampleController extends AbstractController
 {
+
     /**
      * @Route("/example/{number}", name="example_controller")
      */
@@ -19,12 +18,4 @@ class ExampleController extends AbstractController
         $response = new Response("Hello" . $number, Response::HTTP_OK);
         return $response;
     }
-
-    // /**
-    //  * @Route("/file", name="file_controller")
-    //  */
-    //     public function file(): Response
-    //     {
-    //         // return $this->file('/home/umar/Desktop/Symfony Projects/SymfonyParactice/public/Generate_Certificate_1630829208046.pdf');
-    //     }
 }
