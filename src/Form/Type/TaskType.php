1c445746527395dc9ad88e89fd5c3f8570efd5a3
Muhammad Umar <umar.roulia@gmail.com>
2021-09-09 09:08:30 +0500

    Task Form Complete

---
+++ src/Form/Type/TaskType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class, [
+                'required' => $options['require_due_date'],
+                'label' => 'Date before it should be complete',
+            ])
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+            'require_due_date' => false,
+        ]);
+        $resolver->setAllowedTypes('require_due_date', 'bool');
+    }
+}
